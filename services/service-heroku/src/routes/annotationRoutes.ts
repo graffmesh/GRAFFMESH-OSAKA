44f6c82ef6e163706ad6ebdc24f74a591f5c0282
Susumu Fujii <powermac.g7@gmail.com>
2025-03-20 04:52:25 +0900

    feat: separeted sercices

--- services/service-heroku/src/routes/annotationRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import AnnotationController from '../controllers/annotationController';
-
-router.post('/annotations', AnnotationController.createAnnotation);
-router.get('/annotations', AnnotationController.getAnnotations);
-router.delete('/annotations', AnnotationController.deleteAnnotation);
-
-export default router;
