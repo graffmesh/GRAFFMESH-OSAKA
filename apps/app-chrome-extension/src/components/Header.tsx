bd1dbb37cc4b0b529d0bf451fc05f02b0c85fcb8
Susumu Fujii <powermac.g7@gmail.com>
2025-03-20 04:36:23 +0900

    feat: delete apps

--- apps/app-chrome-extension/src/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Header: React.FC = () => <h1>P2P Annotation Tool</h1>;
-
-export default Header;
--- apps/app-chrome-extension/src/utils/debounce.ts
+++
@@ -1,7 +0,0 @@
-export const debounce = (func: Function, delay: number) => {
-  let timeout: NodeJS.Timeout;
-  return (...args: any[]) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), delay);
-  };
-};
